1af135b388f7df7f8ab9841f6e66c864e23974c2
Devashish Chakraborty <devashishchakra@gmail.com>
2023-08-24 15:07:53 +0530

    Made the form functional to add Todo Items in the List

--- index.js
+++ index.js
@@ -9,17 +9,68 @@ class Todo{
 
 class TodoList{
     constructor(){
+        this.list = {}
+    }
 
+    addProject(projectName, project){
+        this.list[projectName] = project;
+    }
+
+    removeProject(projectName){
+        delete this.list[projectName];
+    }
+
+    getProject(projectName){
+        return this.list[projectName];
     }
 }
 
 class Project{
-    constructor(name){
-        this.name = name;
-        this.list = [];
+    constructor(){
+        this.itemList = [];
     }
 
     addTodoItem(todoItem){
-        this.list.push(todoItem);
+        this.itemList.push(todoItem);
+    }
+
+    removeTodoItem(itemIndex){
+        this.itemList.splice(itemIndex);
     }
 }
+
+const todoList = new TodoList();
+todoList.addProject("Home", new Project());
+
+// DOM Stuff
+const addTodoItemBtn = document.querySelector("#addTaskBtn");
+const addTodoFormOverlay = document.querySelector(".addTaskFormOverlay");
+const newTodoTaskForm = document.querySelector(".newTask");
+
+function addTodoBtnHandler(){
+    addTodoFormOverlay.classList.remove("inactive");
+}
+
+function addNewTodoFormHandler(e){
+    todoList.getProject(project.value).addTodoItem(new Todo(title.value, description.value, dueDate.value, priority.value));
+    addTodoFormOverlay.classList.add("inactive");
+    console.log(todoList);
+
+    title.value = "";
+    description.value = "";
+    dueDate.value = "";
+
+    e.preventDefault();
+}
+
+function addTodoFormCloseBtnHandler(){
+    addTodoFormOverlay.classList.add("inactive");
+}
+
+function addNewTodo(){
+    addTodoItemBtn.addEventListener("click", addTodoBtnHandler);
+    newTodoTaskForm.addEventListener("submit", e => addNewTodoFormHandler(e));
+    newTodoTaskForm.addEventListener("reset", addTodoFormCloseBtnHandler);
+}
+
+addNewTodo();
